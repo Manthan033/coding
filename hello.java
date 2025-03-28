548cb7a43af19420fbb80dbe81916b83f8b81378
Manthan033 <manthanrokhade03@gmail.com>
2025-03-28 12:19:46 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-class Test
+class Test;;
 {
-    public static void main(String[] args)
+    public static void main(String[] args){;
     {
         System.out.println("My First Java Program.");
     }
